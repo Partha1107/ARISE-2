0e7b7239358bd3e2ecad01d6415a7825e18e7e2d
Partha <rajashwin@gmail.com>
2026-07-17 11:29:27 +0530

    Checking number 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white text-4xl">
-      ARISE is working 🚀
-    </div>
-  );
+    
+    return <AppRoutes />;
+
 }
 
 export default App;
